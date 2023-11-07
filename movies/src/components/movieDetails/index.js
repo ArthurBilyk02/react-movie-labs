d3823a6fd4530def3e9e266ed3b50d112eebfe98
Arthur <20097938@wit.ie>
2023-11-07 00:19:53 +0000

    Movie Details Page - included production countries

--- movies/src/components/movieDetails/index.js
+++ movies/src/components/movieDetails/index.js
@@ -57,6 +57,21 @@ const MovieDetails = ( props) => {
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
+
+      <Paper 
+        component="ul" 
+        sx={{...root}}
+      >
+        <li>
+          <Chip label="Production Countries" sx={{...chip}} color="primary" />
+        </li>
+        {movie.production_countries.map((country) => (
+          <li>
+            <Chip label={country.name} sx={{...chip}} />
+          </li>
+        ))}
+      </Paper>
+
       <Fab
         color="secondary"
         variant="extended"
